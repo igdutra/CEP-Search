c3652bfe69aac8a414555617320f9d10fd26a624
Ivo Dutra <ivogdutra@gmail.com>
2024-01-28 18:04:31 -0300

    Extract makeSUT helper

--- CEPSearch/CEPiOSTests/CEP Details/CEPDetailsViewControllerTests.swift
+++ CEPSearch/CEPiOSTests/CEP Details/CEPDetailsViewControllerTests.swift
@@ -12,20 +12,30 @@ import CEPSearch
 final class CEPDetailsViewControllerTests: XCTestCase {
     
     func test_labelTexts_withKnownViewData() {
-        // Given known view data
         let viewData = CEPDetailsViewData(cepText: "CEP: 12345-678",
                                           addressText: "Address: Example Street, Apt 101",
                                           districtText: "District: Example District",
                                           cityStateText: "City/State: Example City, EX")
         
-        let viewController = CEPDetailsViewController(viewData: viewData)
+        let sut = makeSUT(viewData: viewData)
         
         // Force view lifecycle
-        viewController.loadViewIfNeeded()
+        sut.loadViewIfNeeded()
         
-        XCTAssertEqual(viewController.cepLabel.text, viewData.cepText)
-        XCTAssertEqual(viewController.addressLabel.text, viewData.addressText)
-        XCTAssertEqual(viewController.districtLabel.text, viewData.districtText)
-        XCTAssertEqual(viewController.cityStateLabel.text, viewData.cityStateText)
+        XCTAssertEqual(sut.cepLabel.text, viewData.cepText)
+        XCTAssertEqual(sut.addressLabel.text, viewData.addressText)
+        XCTAssertEqual(sut.districtLabel.text, viewData.districtText)
+        XCTAssertEqual(sut.cityStateLabel.text, viewData.cityStateText)
     }
 }
+
+// MARK: - Helpers
+
+private extension CEPDetailsViewControllerTests {
+    func makeSUT(viewData: CEPDetailsViewData) -> CEPDetailsViewController {
+        let viewController = CEPDetailsViewController(viewData: viewData)
+        trackForMemoryLeaks(viewController)
+        return viewController
+    }
+}
+
