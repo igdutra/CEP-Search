ed7ab35b8c60926b53f2a90d59afffe0f2e6481e
Ivo Dutra <ivogdutra@gmail.com>
2024-01-28 15:18:59 -0300

    Update to iOS 17

--- CEPSearch/CEPiOSTests/Helpers/UIViewController+Snapshot.swift
+++ CEPSearch/CEPiOSTests/Helpers/UIViewController+Snapshot.swift
@@ -26,17 +26,17 @@ struct SnapshotConfiguration {
             size: CGSize(width: 375, height: 667),
             safeAreaInsets: UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0),
             layoutMargins: UIEdgeInsets(top: 20, left: 16, bottom: 0, right: 16),
-            traitCollection: UITraitCollection(traitsFrom: [
-                .init(forceTouchCapability: .available),
-                .init(layoutDirection: .leftToRight),
-                .init(preferredContentSizeCategory: contentSize),
-                .init(userInterfaceIdiom: .phone),
-                .init(horizontalSizeClass: .compact),
-                .init(verticalSizeClass: .regular),
-                .init(displayScale: 2),
-                .init(displayGamut: .P3),
-                .init(userInterfaceStyle: style)
-            ]))
+            traitCollection: UITraitCollection().modifyingTraits { traits in
+                traits.forceTouchCapability = .available
+                traits.layoutDirection = .leftToRight
+                traits.preferredContentSizeCategory = contentSize
+                traits.userInterfaceIdiom = .phone
+                traits.horizontalSizeClass = .compact
+                traits.verticalSizeClass = .regular
+                traits.displayScale = 2
+                traits.displayGamut = .P3
+                traits.userInterfaceStyle = style
+            })
     }
 }
 
@@ -57,7 +57,7 @@ private final class SnapshotWindow: UIWindow {
     }
     
     override var traitCollection: UITraitCollection {
-        return UITraitCollection(traitsFrom: [super.traitCollection, configuration.traitCollection])
+        configuration.traitCollection
     }
     
     func snapshot() -> UIImage {
