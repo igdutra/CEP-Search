e30bfbbff66d4ccd1730d733f716e996e95a5a03
Ivo Dutra <ivogdutra@gmail.com>
2024-01-28 13:21:22 -0300

    Add `CEPDetails`

--- CEPSearch/CEPSearch/CEP Feature/CEPDetails.swift
+++ CEPSearch/CEPSearch/CEP Feature/CEPDetails.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct CEPDetails {
+    let cep: String
+    let street: String
+    let complement: String
+    let district: String
+    let city: String
+    let state: String
+}
