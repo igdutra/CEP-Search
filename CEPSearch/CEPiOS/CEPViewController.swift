64daf2e853365720b06eb16a6aaa40c2d3a44d35
Ivo Dutra <ivogdutra@gmail.com>
2024-01-28 13:02:57 -0300

    Add specific framework to held iOS specific components

---
+++ CEPSearch/CEPiOS/CEPViewController.swift
@@ -0,0 +1,8 @@
+//
+//  CEPViewController.swift
+//  CEPiOS
+//
+//  Created by Ivo on 28/01/24.
+//
+
+import Foundation
---
+++ CEPSearch/CEPiOSTests/CEPiOSTests.swift
@@ -0,0 +1,13 @@
+//
+//  CEPiOSTests.swift
+//  CEPiOSTests
+//
+//  Created by Ivo on 28/01/24.
+//
+
+import XCTest
+@testable import CEPiOS
+
+final class CEPiOSTests: XCTestCase {
+
+}
