473f4ca929c31e15d8cbd2c21843862970ff1a7d
Ivo Dutra <ivogdutra@gmail.com>
2024-01-28 16:00:35 -0300

    Assert that no request is made on view initialization

--- CEPSearch/CEPiOSTests/CEP Search/CEPSearchViewTests.swift
+++ CEPSearch/CEPiOSTests/CEP Search/CEPSearchViewTests.swift
@@ -6,10 +6,17 @@
 //
 
 import XCTest
+import CEPiOS
 
 final class CEPSearchViewTests: XCTestCase {
 
-    func test() {
-        XCTFail("Initial failing test.")
+    func test_init_doesNotRequestCEP() {
+        let spy = CEPGetterSpy()
+        let view = CEPSearchViewContainer(cepGetter: spy,
+                                          placeholderText: .init(),
+                                          buttonText: .init())
+        
+        XCTAssertEqual(spy.receivedMessages, [], "Expected no CEP request on view initialization")
     }
+
 }
--- CEPSearch/CEPiOSTests/CEP Search/Helpers/CEPGetterSpy.swift
+++ CEPSearch/CEPiOSTests/CEP Search/Helpers/CEPGetterSpy.swift
@@ -5,4 +5,24 @@
 //  Created by Ivo on 28/01/24.
 //
 
-import Foundation
+import CEPSearch
+
+final class CEPGetterSpy: CEPGetter {
+    enum ReceivedMessage: Equatable {
+        case getDetails(String)
+    }
+    private(set) var receivedMessages = [ReceivedMessage]()
+    
+    // MARK: - Interface
+
+    func getCEPDetails(for cep: String) async throws -> CEPDetails {
+        receivedMessages.append(.getDetails(cep))
+        
+        return CEPDetails(cep: .init(),
+                          street: .init(),
+                          complement: .init(),
+                          district: .init(),
+                          city: .init(),
+                          state: .init())
+    }
+}
