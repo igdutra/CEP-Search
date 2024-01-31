1ba848f1a3bd3875c9635856acb49240340013c6
Ivo Dutra <ivogdutra@gmail.com>
2024-01-31 12:37:22 -0300

    Create `CEPApp` project

---
+++ CEPApp/CEPApp/CEPAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CEPAppApp.swift
+//  CEPApp
+//
+//  Created by Ivo on 31/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CEPAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
