75520227951d8388323423a4e47a87de0b5483aa
Ivo Dutra <ivogdutra@gmail.com>
2024-01-31 13:07:20 -0300

    Create Compositon Root: First time running the application

--- CEPApp/CEPApp/CEPAppApp.swift
+++ CEPApp/CEPApp/CEPAppApp.swift
@@ -6,12 +6,31 @@
 //
 
 import SwiftUI
+import CEPSearch
+import CEPiOS
 
 @main
 struct CEPAppApp: App {
+    
+    let searchView: CEPSearchView
+    
+    // MARK: - Compositon Root: Compose all componets together
+    
+    init() {
+        let baseURL = URL(string: "https://viacep.com.br/ws/")!
+        
+        let session = URLSession(configuration: .default)
+        let client = URLSessionHTTPClient(session: session)
+        let cepGetter = RemoteCEPGetter(baseURL: baseURL, client: client)
+        searchView = CEPSearchUIComposer.composeView(cepGetter: cepGetter)
+    }
+    
+    
+    // MARK: - View
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            searchView
         }
     }
 }
